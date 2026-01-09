6b2314fbcefdf61e700f353f24bcec77a7e55441
Marcel Peschka <marcelpeschka@peschi.info>
2026-01-09 20:24:32 +0100

    Update version to 2.20.0

--- darts-caller.py
+++ darts-caller.py
@@ -63,7 +63,7 @@ main_directory = os.path.dirname(os.path.realpath(__file__))
 parent_directory = os.path.dirname(main_directory)
 
 
-VERSION = '2.19.13'
+VERSION = '2.20.0'
 
 
 DEFAULT_EMPTY_PATH = ''
